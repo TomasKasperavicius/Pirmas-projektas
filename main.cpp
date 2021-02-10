3b593edd4637b7d31f4371dd9c289eda7e8e9b21
TomasKasperavicius <78591148+TomasKasperavicius@users.noreply.github.com>
2021-02-10 19:22:32 +0200

    Sukurta galutinė (v1.0) programos versija

--- main.cpp
+++ main.cpp
@@ -7,9 +7,18 @@ using std::endl;
 
 int main()
 {
-    std::string vardas, pirma_eil, antra_eil, trecia_eil, ketvirta_eil, penkta_eil, pasisveikinimas;
+    std::string vardas, pasisveikinimas;
+    int remelio_plotis;
     cout << "Iveskite savo varda:"<< endl;
     cin >> vardas;
+    std::string zinute="Iveskite remelio ploti:";
+    do
+    {     
+        cout << zinute << endl;
+        cin >> remelio_plotis;
+        zinute="Plotis turi buti teigiamas skaicius!";
+    } while (remelio_plotis<0);
+    std::string eilute;
     if(vardas[vardas.size()-1]=='s')
     {
         pasisveikinimas="Sveikas, ";
@@ -19,25 +28,63 @@ int main()
     {
         pasisveikinimas="Sveika, ";
     }
-    trecia_eil="* "+pasisveikinimas+vardas+"! *";
-    for(int i=0;i<trecia_eil.size();i++)
+    for(int i=0;i<remelio_plotis;i++)
     {
-        pirma_eil+="*";
-        if(i!=0&&i!=trecia_eil.size()-1)
+
+    }
+    eilute="*"+pasisveikinimas+vardas+"!"+"*";
+    if(remelio_plotis==0)
+    {
+        for(int i=0;i<eilute.size()+remelio_plotis;i++)
         {
-            antra_eil+=" ";
+            cout << "*";
         }
-        else
+        cout << endl;
+        cout <<eilute<<endl;
+        for(int i=0;i<eilute.size()+remelio_plotis;i++)
         {
-            antra_eil+="*";
+            cout << "*";
+        }
+        cout << endl;
+    }
+    else
+    {
+        std::string tarpai;
+        for(int i=0;i<remelio_plotis;i++)
+        {
+            tarpai+=" ";
+        }
+        eilute="*"+tarpai+pasisveikinimas+vardas+"!"+tarpai+"*";
+        for(int i=0;i<remelio_plotis*2+3;i++)
+        {
+            if(i==0||i==remelio_plotis*2+3-1)
+            {
+                for(int j=0;j<eilute.size();j++)
+                {
+                cout << "*";
+                }
+                cout << endl;
+            }
+            else if(i==remelio_plotis+1)
+            {
+                cout << eilute <<endl;
+            }
+            else
+            {
+                for(int j=0;j<eilute.size();j++)
+                {
+                    if (j==0||j==eilute.size()-1)
+                    {
+                        cout << "*";
+                    }
+                    else
+                    {
+                        cout << " ";
+                    }    
+                }
+                cout << endl;
+            }            
         }
     }
-    ketvirta_eil=antra_eil;
-    penkta_eil=pirma_eil;
-    cout <<pirma_eil<<endl;
-    cout <<antra_eil<<endl;
-    cout <<trecia_eil<<endl;
-    cout <<ketvirta_eil<<endl;
-    cout <<penkta_eil<<endl;
     return 0;
 }
